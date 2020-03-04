4b7307d822462825fecf62254e74e275673f4f51
modi98 <mauricio.alvaradoh98@gmail.com>
2020-03-04 10:07:00 -0800

    Added vehicle model.

---
+++ src/models/vehicle.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const vehicleSchema = new mongoose.Schema({
+  model: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: String,
+    required: true
+  },
+  license_plate: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    virtuals: true,
+    transform: (obj, ret) => { delete ret._id }
+  }
+});
+
+vehicleSchema.methods = {
+  view() {
+    return {
+      id: this.id,
+      model: this.model,
+      year: this.year,
+      license_plate: this.license_plate
+    }
+  }
+}
+
+module.exports = mongoose.model('Driver', vehicleSchema);
